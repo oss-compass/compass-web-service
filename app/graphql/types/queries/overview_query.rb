8fc1ff84883a463fdf004bfdba805a03e54743cd
EdmondFrank <EdmondFrank@hotmail.com>
2022-11-09 13:47:37 +0800

    Alter overview hot projects display strategy
    Signed-off-by: EdmondFrank <EdmondFrank@hotmail.com>

--- app/graphql/types/queries/overview_query.rb
+++ app/graphql/types/queries/overview_query.rb
@@ -49,8 +49,64 @@ module Types
           skeleton['dimensions_count'] = DIMENSIONS_COUNT
           skeleton['models_count'] = MODELS_COUNT
           skeleton['metrics_count'] = METRICS_COUNT
-          resp = GithubRepo.only(GithubFixedTemplates)
-          resp2 = GiteeRepo.only(GiteeFixedTemplates)
+
+          activity_top50_last_month =
+            ActivityMetric
+              .custom(collapse: { field: 'label.keyword' })
+              .range(:grimoire_creation_date, gte: DateTime.now() - 1.month, lte: DateTime.now())
+              .page(1)
+              .per(50)
+              .sort(activity_score: :desc)
+              .source(['label'])
+              .execute
+              .raw_response['hits']['hits'].map{ |row| row['_source']['label'] }
+
+          activity_upward_trending =
+            ActivityMetric
+              .range(:grimoire_creation_date, gte: DateTime.now() - 1.month, lte: DateTime.now())
+              .sort(grimoire_creation_date: :desc)
+              .page(0)
+              .per(0)
+              .aggregate(
+                {
+                  label_group: {
+                    terms: {
+                      field: "label.keyword",
+                      size: 100
+                    },
+                    aggs: {
+                      arise: {
+                        date_histogram: {
+                          field: :grimoire_creation_date,
+                          interval: "week",
+                        },
+                        aggs: {
+                          avg_activity: {
+                            avg: {
+                              field: "activity_score"
+                            }
+                          },
+                          the_delta: {
+                            derivative: {
+                              buckets_path: "avg_activity"
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                })
+              .execute
+              .aggregations&.[]('label_group')&.[]('buckets')
+              .select { |row| row['arise']['buckets'].last&.[]('the_delta')&.[]('value').to_f > 0.0 }
+              .map { |row| row['key'] }
+
+          candidate_set = (activity_upward_trending || []) + (activity_top50_last_month || [])
+
+          gitee_repos = candidate_set.select {|row| row =~ /gitee\.com/ }.sample(12)
+          github_repos = candidate_set.select {|row| row =~ /github\.com/ }.sample(12)
+          resp = GithubRepo.only(github_repos)
+          resp2 = GiteeRepo.only(gitee_repos)
           skeleton['trends'] = build_github_repo(resp).map { |repo| OpenStruct.new(repo) }
           skeleton['trends'] += build_gitee_repo(resp2).map { |repo| OpenStruct.new(repo) }
           skeleton
