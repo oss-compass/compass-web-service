2d602fc722d915149d5176a88e119f1d1d9a2b99
EdmondFrank <EdmondFrank@hotmail.com>
2022-11-29 10:32:59 +0800

    Fix group_activity_metric ignore is_org field
    Signed-off-by: EdmondFrank <EdmondFrank@hotmail.com>

--- app/models/group_activity_metric.rb
+++ app/models/group_activity_metric.rb
@@ -3,6 +3,23 @@ class GroupActivityMetric < BaseMetric
     "#{MetricsIndexPrefix}_group_activity"
   end
 
+  def self.query_label_one(label, level)
+    Rails.cache.fetch(
+      "#{self.name}-#{__method__}-#{label}-#{level}",
+      expires_in: CacheTTL
+    ) do
+      self
+        .must(match: { 'label.keyword': label })
+        .where(is_org: true)
+        .where(level: level)
+        .page(1)
+        .per(1)
+        .sort(grimoire_creation_date: :desc)
+        .execute
+        .raw_response
+    end
+  end
+
   def self.aggs_repo_by_date(repo_url, begin_date, end_date, aggs)
     Rails.cache.fetch(
       "#{self.name}-#{__method__}-#{repo_url}-#{begin_date}-#{end_date}",
