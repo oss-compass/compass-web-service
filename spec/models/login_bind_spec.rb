fed1b68e5e453178a3e66390f29133979bf733a5
魏宏斌 <1602586227@qq.com>
2023-05-10 15:52:23 +0800

    feat: create_project_task,create_repo_task Change verification, add logout login, logout account
    Signed-off-by: 魏宏斌 <1602586227@qq.com>

--- spec/models/login_bind_spec.rb
+++ spec/models/login_bind_spec.rb
@@ -6,20 +6,19 @@
 #  user_id     :bigint           not null
 #  provider    :string(255)      not null
 #  account     :string(255)      not null
-#  status      :integer          default(0), not null
 #  nickname    :string(255)
 #  avatar_url  :string(255)
-#  parent_id   :string(255)
+#  uid         :string(255)
 #  provider_id :string(255)
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
 # Indexes
 #
-#  index_login_binds_on_account                 (account)
-#  index_login_binds_on_parent_id_and_provider  (parent_id,provider) UNIQUE
-#  index_login_binds_on_provider                (provider)
-#  index_login_binds_on_user_id                 (user_id)
+#  index_login_binds_on_account              (account)
+#  index_login_binds_on_provider             (provider)
+#  index_login_binds_on_uid_and_provider_id  (uid,provider_id) UNIQUE
+#  index_login_binds_on_user_id              (user_id)
 #
 require 'rails_helper'
 
