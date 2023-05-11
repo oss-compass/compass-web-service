51f46ea109ec0eb19572c1a63a32b2b20809e25b
魏宏斌 <1602586227@qq.com>
2023-05-11 18:55:35 +0800

    feat: send email limit
    Signed-off-by: 魏宏斌 <1602586227@qq.com>

---
+++ app/graphql/mutations/send_email_verify.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Mutations
+  class SendEmailVerify < BaseMutation
+    field :status, String, null: false
+
+    def resolve
+      current_user = context[:current_user]
+      raise GraphQL::ExecutionError.new I18n.t('users.require_login') if current_user.blank?
+
+      raise GraphQL::ExecutionError.new I18n.t('users.email_verified') if current_user.email_verified?
+
+      current_user.send_email_verification
+
+      full_messages = current_user.errors.full_messages
+      raise GraphQL::ExecutionError.new full_messages.first if full_messages.present?
+
+      OpenStruct.new({ status: true, message: '' })
+    rescue => ex
+      OpenStruct.new({ status: false, message: ex.message })
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,8 @@ class User < ApplicationRecord
 
   has_many :login_binds, dependent: :destroy
 
+  validate :check_email_change_limit
+
   after_update :send_email_verification, if: -> { saved_changes.keys.include?('email') }
 
   ANONYMOUS_EMAIL_SUFFIX = '@user.anonymous.oss-compass.org'
@@ -54,6 +56,8 @@ class User < ApplicationRecord
   end
 
   def send_email_verification
+    return unless check_send_email_limit
+
     generate_email_verification_token
     UserMailer.with(
       user: self,
@@ -133,5 +137,37 @@ class User < ApplicationRecord
 
   def self.gen_anonymous_email(provider, uid)
     "#{provider}_#{uid}#{ANONYMOUS_EMAIL_SUFFIX}"
+  end
+
+  private
+
+  def email_change_limit_key
+    "user:email_change_limit:#{id}"
+  end
+
+  def send_email_limit_key
+    "user:send_email_limit:#{id}:#{email}"
+  end
+
+  def check_send_email_limit
+    count = Rails.cache.increment(send_email_limit_key)
+    Rails.cache.write(send_email_limit_key, 1, expires_in: 1.day, raw: true) if count == 1
+    max_count = ENV.fetch('MAX_SEND_EMAIL_COUNT') { 3 }.to_i
+    if count > max_count
+      errors.add(:base, I18n.t("users.send_email_limit", count: max_count))
+      return false
+    end
+    true
+  end
+
+  def check_email_change_limit
+    return if new_record?
+
+    return unless email_changed?
+
+    count = Rails.cache.increment(email_change_limit_key)
+    Rails.cache.write(email_change_limit_key, 1, expires_in: 1.day, raw: true) if count == 1
+    max_count = ENV.fetch('MAX_EMAIL_CHANGE_COUNT') { 3 }.to_i
+    errors.add(:base, I18n.t("users.email_change_limit", count: max_count)) if count > max_count
   end
 end
