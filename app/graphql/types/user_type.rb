c826ab78f0c3a4456d38c767a11826d332c20844
魏宏斌 <1602586227@qq.com>
2023-05-11 17:39:39 +0800

    feat: UserType add field email_verified
    Signed-off-by: 魏宏斌 <1602586227@qq.com>

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,6 +4,11 @@ module Types
     field :login_binds, [Types::LoginBindType]
     field :name, String, null: false
     field :email, String, null: false
+    field :email_verified, Boolean, null: false
+
+    def email_verified
+      object.email_verified?
+    end
 
     def email
       object.anonymous? ? '' : object.email
