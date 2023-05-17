19383f9837167d918abed954513299646b36d189
魏宏斌 <1602586227@qq.com>
2023-05-17 16:53:44 +0800

    feat: update verify email redirect url
    Signed-off-by: 魏宏斌 <1602586227@qq.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@ class UsersController < ApplicationController
     token = params[:token]
     user = User.find_by(email_verification_token: token)
     if user.present? && user.verify_email(token)
-      redirect_to url_for('/pages/auth/email/verify/success')
+      redirect_to url_for('/auth/email/verify/success')
     else
-      redirect_to url_for('/pages/auth/email/verify/failed')
+      redirect_to url_for('/auth/email/verify/failed')
     end
   end
 end
