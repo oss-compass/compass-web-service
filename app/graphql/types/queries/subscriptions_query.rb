a0613bde675875b9fff83d80ee12414e11fd1e55
魏宏斌 <1602586227@qq.com>
2023-05-23 11:25:31 +0800

    feat: Added Queries::SubscriptionsQuery UserType subscriptions to support label and level filtering
    Signed-off-by: 魏宏斌 <1602586227@qq.com>

---
+++ app/graphql/types/queries/subscriptions_query.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Queries
+  class SubscriptionsQuery < BaseQuery
+
+    argument :label, String, required: true, description: 'repo or project label'
+    argument :level, String, required: true, description: 'repo or project level(repo/community)'
+
+    def resolve(label: nil, level: nil)
+      subscriptions = object.subscriptions.order(id: :desc)
+      subscriptions = subscriptions.where(subject: Subject.find_by(label: label)) if label.present?
+      subscriptions = subscriptions.where(subject: Subject.where(level: level)) if level.present?
+      subscriptions
+    end
+  end
+end
