3fb08050d81163e0216c69e9f8cfed80c65f7e0f
魏宏斌 <1602586227@qq.com>
2023-05-23 11:41:37 +0800

    feat: Types::BaseConnection add total_count
    Signed-off-by: 魏宏斌 <1602586227@qq.com>

--- app/graphql/types/base_connection.rb
+++ app/graphql/types/base_connection.rb
@@ -2,5 +2,15 @@ module Types
   class BaseConnection < Types::BaseObject
     # add `nodes` and `pageInfo` fields, as well as `edge_type(...)` and `node_nullable(...)` overrides
     include GraphQL::Types::Relay::ConnectionBehaviors
+
+    field :total_count, Int, null: false
+
+    def total_count
+      return context[:connection_total_count] if context[:connection_total_count].present?
+
+      return object.items.count if object.items.is_a?(Array)
+
+      object.items&.unscope(:offset)&.count
+    end
   end
 end
