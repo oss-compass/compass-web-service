b936908728274526f73e32fbcff857ba32c8c79c
魏宏斌 <1602586227@qq.com>
2023-05-24 16:30:54 +0800

    feat: Notification logic adjustment
    Signed-off-by: 魏宏斌 <1602586227@qq.com>

--- app/workers/subscriptions_update_worker.rb
+++ app/workers/subscriptions_update_worker.rb
@@ -16,17 +16,32 @@ class SubscriptionsUpdateWorker
     count = message['count']
     status_updated_at = message['status_updated_at']
 
-    subject = Subject.find_or_create_by(label: label) do |subject|
-      subject.level = level
-      subject.status = status
-      subject.count = count
-      subject.status_updated_at = status_updated_at
+    subject = Subject.find_by(label: label)
+    if subject.blank?
+      subject = Subject.create(
+        label: label,
+        level: level,
+        status: status,
+        status_updated_at: status_updated_at,
+        count: count
+      )
+    end
+    subject.status != status && subject.update(status: status)
+
+    notification_flag = true
+    case status
+    when Subject::COMPLETE
+      notification_type = NotificationService::SUBSCRIPTION_UPDATE
+    when message['status'] == ProjectTask::Pending
+      notification_type = NotificationService::SUBMISSION
+    else
+      notification_flag = false
     end
 
-    notification_type = message['status'] == ProjectTask::Pending ? NotificationService::SUBMISSION : NotificationService::SUBSCRIPTION_UPDATE
-
-    subject.subscriptions.includes(:user).each do |subscription|
-      NotificationService.new(subscription.user, notification_type, { subject: subject }).execute
+    if notification_flag
+      subject.subscriptions.includes(:user).each do |subscription|
+        NotificationService.new(subscription.user, notification_type, { subject: subject }).execute
+      end
     end
 
     ack!
