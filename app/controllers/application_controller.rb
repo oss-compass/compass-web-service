83ed31bebefe683180c1b6ff06243e6f45f3f1a9
EdmondFrank <EdmondFrank@hotmail.com>
2023-08-24 22:09:25 +0800

    Refactor user controller redirect_to
    Signed-off-by: EdmondFrank <EdmondFrank@hotmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -82,6 +82,19 @@ class ApplicationController < ActionController::Base
     Rails.logger.error("Failed to notify on pr #{pr_number}, #{ex.message}")
   end
 
+  def redirect_url(error: nil, default_url: nil, skip_cookies: false)
+    default_host = Addressable::URI.parse(ENV['DEFAULT_HOST'])
+    url = cookies['auth.callback-url'].presence || default_url
+    url = default_url if skip_cookies
+    uri = Addressable::URI.parse(url)
+    uri.scheme = default_host.scheme
+    uri.host = default_host.host
+    if error.present?
+      uri.query_values = uri.query_values.to_h.merge({ error: error, ts: (Time.now.to_f * 1000).to_i })
+    end
+    uri.to_s
+  end
+
   private
 
   def user_agent
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,14 @@ class UsersController < ApplicationController
     token = params[:token]
     user = User.find_by(email_verification_token: token)
     if user.present? && user.verify_email(token)
-      redirect_to url_for('/auth/email/verify/success')
+      redirect_to url_for(redirect_url(default_url: '/auth/email/verify/success'))
     else
-      redirect_to url_for('/auth/email/verify/failed')
+      redirect_to url_for(redirect_url(default_url: '/auth/email/verify/failed'))
     end
   end
 
   def accept_invitation
-    redirect_to url_for('/auth/signin') unless current_user.present?
+    redirect_to url_for(redirect_url(default_url: '/auth/signin')) unless current_user.present?
     token = params[:token]
     invitation = LabModelInvitation.find_by(token: token)
     error = nil
@@ -18,11 +18,9 @@ class UsersController < ApplicationController
     ok, error = invitation.verify_and_finish!(current_user, token) if error.blank?
 
     if error.present?
-      uri = Addressable::URI.parse('/lab/model/my')
-      uri.query_values = uri.query_values.to_h.merge({ error: error, ts: (Time.now.to_f * 1000).to_i })
-      redirect_to url_for(uri.to_s)
+      redirect_to url_for(redirect_url(error: error, default_url: '/lab/model/my'))
     else
-      redirect_to url_for("/lab/model/#{invitation.lab_model.id}/user")
+      redirect_to url_for(redirect_url(default_url: "/lab/model/#{invitation.lab_model.id}/user"))
     end
   end
 end
