ce4c6cfca1b4f3b8c418ac7d6459dd87ab4ff036
EdmondFrank <EdmondFrank@hotmail.com>
2023-08-24 21:52:03 +0800

    Fix typo
    Signed-off-by: EdmondFrank <EdmondFrank@hotmail.com>

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -108,7 +108,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     url = cookies['auth.callback-url'].presence || default_url
     url = default_url if skip_cookies
     uri = Addressable::URI.parse(url)
-    uri.scheme = 'http'
+    uri.scheme = default_host.scheme
     uri.host = default_host.host
     if error.present?
       uri.query_values = uri.query_values.to_h.merge({ error: error, ts: (Time.now.to_f * 1000).to_i })
