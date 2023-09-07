476c5f1efd199b4bd0bd851d721e8f473f9a4f0d
EdmondFrank <EdmondFrank@hotmail.com>
2023-09-07 16:31:46 +0800

    Update schema
    Signed-off-by: EdmondFrank <EdmondFrank@hotmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_071253) do
-  create_table "active_storage_attachments", charset: "utf8mb4",
-  force: :cascade do |t| t.string "name", null: false t.string
-  "record_type", null: false t.bigint "record_id", null: false
-  t.bigint "blob_id", null: false t.datetime "created_at", null: false
-  t.index ["blob_id"], name:
-  "index_active_storage_attachments_on_blob_id" t.index
-  ["record_type", "record_id", "name", "blob_id"], name:
-  "index_active_storage_attachments_uniqueness", unique: true end
+ActiveRecord::Schema[7.0].define(version: 2023_09_07_081549) do
+  create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
 
   create_table "active_storage_blobs", charset: "utf8mb4", force: :cascade do |t|
     t.string "key", null: false
@@ -254,7 +255,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_071253) do
   end
 
   create_table "shortened_labels", charset: "utf8mb4", force: :cascade do |t|
-    t.string "label", null: false
+    t.string "label", null: false, collation: "utf8mb4_bin"
     t.string "short_code", null: false
     t.string "level", null: false
     t.datetime "created_at", null: false
