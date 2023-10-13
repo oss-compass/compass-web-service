92fab7dbb2e83110f3f022b6de250874b0ae4f5f
EdmondFrank <EdmondFrank@hotmail.com>
2023-10-13 10:10:03 +0800

    Chore: Add index seed data initialization
    Signed-off-by: EdmondFrank <EdmondFrank@hotmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
+# Initializing Indexes
+indices = [
+  ActivityMetric, ActivitySummary, BaseCollection,
+  CodequalityMetric, CodequalitySummary, CommunityMetric,
+  CommunitySummary, CustomV1Metric, GiteeIssueEnrich,
+  GiteePullEnrich, GiteeRepoEnrich, GiteeRepo,
+  GithubIssueEnrich, GithubPullEnrich, GithubRepoEnrich,
+  GithubRepo, GroupActivityMetric, GroupActivitySummary,
+  StarterProjectHealthMetric
+].each { |indexer| indexer.create_index unless indexer.index_exists? }
+
 metrics_set =
 [
   ## git category metrics
