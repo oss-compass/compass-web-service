f1bb18c520602747ef8fc036fe4bcfb4efe393c5
EdmondFrank <EdmondFrank@hotmail.com>
2024-03-06 11:26:04 +0800

    Add mandatory opts when publish.
    Signed-off-by: EdmondFrank <EdmondFrank@hotmail.com>
    Acked-by: EdmondFrank <EdmondFrank@hotmail.com>

--- config/initializers/rabbitmq.rb
+++ config/initializers/rabbitmq.rb
@@ -4,7 +4,7 @@ class RabbitMQ
     # here env['MQ_CONNECTION'], configured in the environment variable, e.g.: amqp://username:password@rabbitmq_server_ip
     def publish(queue_name, message = {})
       publisher = Sneakers::Publisher.new
-      publisher.publish(message.to_json, to_queue: queue_name)
+      publisher.publish(message.to_json, to_queue: queue_name, mandatory: true)
       publisher.instance_variable_get(:@bunny).close
     end
   end
