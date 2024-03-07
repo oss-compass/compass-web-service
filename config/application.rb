02a17b0373bcd83c60323ea1f7b45ed549114518
EdmondFrank <EdmondFrank@hotmail.com>
2024-03-07 13:54:20 +0800

    Shared cache for overseas and local sites.
    Signed-off-by: EdmondFrank <EdmondFrank@hotmail.com>

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module CompassWebService
       read_timeout:       2, # Defaults to 1 second
       write_timeout:      2, # Defaults to 1 second
       reconnect_attempts: 2,   # Defaults to 0
-      namespace: "#{ENV['DEFAULT_HOST']}:cache"
+      namespace: 'cache'
     }
 
     config.session_store :redis_session_store,
