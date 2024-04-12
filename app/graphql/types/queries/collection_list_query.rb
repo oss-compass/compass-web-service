25c1a837382e330cc9fb565139725c8226a6fee2
EdmondFrank <EdmondFrank@hotmail.com>
2024-04-12 10:02:54 +0800

    Fix orderless of current page of the collection.
    Signed-off-by: EdmondFrank <EdmondFrank@hotmail.com>

--- app/graphql/types/queries/collection_list_query.rb
+++ app/graphql/types/queries/collection_list_query.rb
@@ -14,6 +14,8 @@ module Types
       argument :sort_opts, [Input::SortOptionInput], required: false, description: 'sort options'
 
       def resolve(ident:, level: nil, page: 1, per: 20, keyword: nil, sort_opts: [])
+        support_sort_types = ['label', 'updated_at', 'activity_score']
+        support_sort_directions = ['desc', 'asc']
         resp = BaseCollection.list(ident, level, page, per, keyword, sort_opts)
         count = BaseCollection.count_by(ident, level, keyword)
         labels = resp&.[]('hits')&.[]('hits')&.map{ |item| item['_source']['label'] }
@@ -25,6 +27,21 @@ module Types
           resp2 = GiteeRepo.only(gitee_repos)
           candidates = build_github_repo(resp).map { |repo| OpenStruct.new(repo) }
           candidates += build_gitee_repo(resp2).map { |repo| OpenStruct.new(repo) }
+          if sort_opts.present?
+            sort_opts.each do |sort_opt|
+              if support_sort_types.include?(sort_opt.type) && support_sort_directions.include?(sort_opt.direction)
+                sort_field = sort_opt.type == 'label' ? 'name' : sort_opt.type
+                candidates = candidates.sort_by do |candidate|
+                  if sort_field == 'activity_score'
+                    (candidate['metric_activity']&.first&.activity_score).to_f
+                  else
+                    candidate[sort_field]
+                  end
+                end
+                candidates = candidates.reverse if sort_opt.direction == 'desc'
+              end
+            end
+          end
         else
           []
         end
