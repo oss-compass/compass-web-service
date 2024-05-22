8532ccc0d6b96cb89bce30657faa9ae204ad6c25
lishengbao <lishengbao147@gmail.com>
2024-05-22 19:59:59 +0800

    Fixing comments
    Signed-off-by: lishengbao <lishengbao147@gmail.com>

--- app/graphql/input/repo_extension_input.rb
+++ app/graphql/input/repo_extension_input.rb
@@ -2,10 +2,10 @@
 
 module Input
   class RepoExtensionInput < Types::BaseInputObject
-    argument :repo_name, String, required: true, description: "repo name"
-    argument :repo_technology_type, String, description: "Technical field of the repo"
-    argument :repo_attribute_type, String, description: "repo attribute type, either self-developed or third-party"
-    argument :manager, String, description: "Warehouse manager or administrator"
-    argument :manager_email, String, description: "Warehouse manager email or administrator email"
+    argument :repo_name, String, required: true, description: "Repository name"
+    argument :repo_technology_type, String, description: "Technical field of the Repository"
+    argument :repo_attribute_type, String, description: "Repository attribute type, either self-developed or third-party"
+    argument :manager, String, description: "Repository manager or administrator"
+    argument :manager_email, String, description: "Repository manager email or administrator email"
   end
 end
