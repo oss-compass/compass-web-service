6b810e84f50b2ddc25a74f21c8141d06fa721ea2
lishengbao <lishengbao147@gmail.com>
2024-07-24 10:19:36 +0800

    Add search and sorting functions in tpc_software_selection_report_page interface
    Signed-off-by: lishengbao <lishengbao147@gmail.com>

--- app/graphql/types/queries/tpc/tpc_software_selection_page_query.rb
+++ app/graphql/types/queries/tpc/tpc_software_selection_page_query.rb
@@ -13,8 +13,10 @@ module Types
         argument :selection_type, Integer, required: true, description: 'incubation: 0, sandbox: 1, graduation: 2'
         argument :page, Integer, required: false, description: 'page number'
         argument :per, Integer, required: false, description: 'per page number'
+        argument :filter_opts, [Input::FilterOptionInput], required: false, description: 'filter options'
+        argument :sort_opts, [Input::SortOptionInput], required: false, description: 'sort options'
 
-        def resolve(label: nil, level: nil, selection_type: 0, page: 1, per: 9)
+        def resolve(label: nil, level: nil, selection_type: 0, page: 1, per: 9, filter_opts: nil, sort_opts: nil)
           current_user = context[:current_user]
           login_required!(current_user)
           validate_by_label!(current_user, label)
@@ -23,7 +25,28 @@ module Types
 
           items = []
           if subject.present?
-            items = subject.tpc_software_selections.where(selection_type: selection_type)
+            items = TpcSoftwareSelection.joins(:user)
+                                        .where("tpc_software_selections.selection_type = ?", selection_type)
+                                        .where("tpc_software_selections.subject_id = ?", subject.id)
+            if filter_opts.present?
+              filter_opts.each do |filter_opt|
+                if filter_opt.type == "user"
+                  conditions = filter_opt.values.map { |value| "users.name LIKE ?" }.join(" OR ")
+                  like_values = filter_opt.values.map { |value| "%#{value}%" }
+                else
+                  conditions = filter_opt.values.map { |value| "tpc_software_selections.#{filter_opt.type} LIKE ?" }.join(" OR ")
+                  like_values = filter_opt.values.map { |value| "%#{value}%" }
+                end
+                items = items.where(conditions, *like_values)
+              end
+            end
+            if sort_opts.present?
+              sort_opts.each do |sort_opt|
+                items = items.order("tpc_software_selections.#{sort_opt.type} #{sort_opt.direction}")
+              end
+            else
+              items = items.order("tpc_software_selections.created_at desc")
+            end
           end
 
           pagyer, records = pagy(items, { page: page, items: per })
