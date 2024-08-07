41f380d85417bfff7cc57cf03e191c0bd2516df7
lishengbao <lishengbao147@gmail.com>
2024-08-07 14:33:34 +0800

    Update the required options of the upstream_code_url and lifecycle_policy fields
    Signed-off-by: lishengbao <lishengbao147@gmail.com>

--- app/graphql/input/tpc_software_graduation_report_update_input.rb
+++ app/graphql/input/tpc_software_graduation_report_update_input.rb
@@ -4,10 +4,10 @@ module Input
   class TpcSoftwareGraduationReportUpdateInput < Types::BaseInputObject
     argument :name, String, required: true
     argument :tpc_software_sig_id, Integer, required: true
-    argument :upstream_code_url, String, required: true
+    argument :upstream_code_url, String, required: false
     argument :programming_language, String, required: true
     argument :adaptation_method, String, required: true
-    argument :lifecycle_policy, String, required: false
+    argument :lifecycle_policy, String, required: true
     argument :round_upstream, String, required: false
   end
 end
