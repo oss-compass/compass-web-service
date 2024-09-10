fd482e2aa3f091f5ec84c4f10587ba067040fe3a
lishengbao <lishengbao147@gmail.com>
2024-09-10 18:47:03 +0800

    Add sig_committer in tpc_software_sig_type
    Signed-off-by: lishengbao <lishengbao147@gmail.com>

--- app/graphql/types/tpc/tpc_software_sig_type.rb
+++ app/graphql/types/tpc/tpc_software_sig_type.rb
@@ -6,6 +6,7 @@ module Types
       field :id, Integer, null: false
       field :name, String, null: false
       field :description, String, null: false
+      field :sig_committer, [Types::Tpc::TpcSoftwareMemberType]
     end
   end
 end
