09e91adc9d3f74753b37b645e2b224173963e2ef
lisongnan <lcamn7700@outlook.com>
2025-05-14 20:32:48 +0800

    add  search_grimoire Signed-off-by: lisongnan <lcamn7700@outlook.com>

--- app/api/openapi/shared_params/search.rb
+++ app/api/openapi/shared_params/search.rb
@@ -5,27 +5,39 @@ module Openapi
       extend Grape::API::Helpers
 
       params :search do
-        requires :label, type: String, desc: 'repo or community label / 仓库或社区标签', documentation: { param_type: 'body',example: 'https://github.com/oss-compass/compass-web-service' }
-        optional :level, type: String, desc: 'level (repo/community), default: repo / 层级', documentation: { param_type: 'body',example: 'repo' }
-        optional :filter_opts, type: Array, desc: 'filter options / 筛选条件', documentation: { param_type: 'body' } do
-          requires :type, type: String, desc: 'filter option type / 筛选类型'
-          requires :values, type: Array, desc: 'filter option values / 筛选值'
-        end
-        optional :sort_opts, type: Array, desc: 'sort options / 排序条件', documentation: { param_type: 'body' ,example: [{ type: 'created_at', direction:  "desc" }]} do
-          requires :type, type: String, desc: 'sort type / 排序类型'
-          requires :direction, type: String, desc: 'sort direction (asc/desc) / 排序方向'
-        end
-        optional :begin_date, type: DateTime, desc: 'begin date / 开始日期', documentation: { param_type: 'body' }
-        optional :end_date, type: DateTime, desc: 'end date / 结束日期', documentation: { param_type: 'body' }
-        optional :page, type: Integer, default: 1, desc: 'page number / 页码', documentation: { param_type: 'body' }
-        optional :size, type: Integer, default: 20, desc: 'page size / 每页条数', documentation: { param_type: 'body' }
+        requires :label, type: String, desc: '仓库或社区地址', documentation: { param_type: 'body', example: 'https://github.com/oss-compass/compass-web-service' }
+        optional :level, type: String, desc: '层级 repo或community', default: 'repo', documentation: { param_type: 'body', example: 'repo' }
+
+        optional :sort, type: String, desc: '排序项', documentation: { param_type: 'body', example: 'created_at' }
+        optional :direction, type: String, desc: '排序方向', documentation: { param_type: 'body', example: 'desc' }
+        optional :begin_date, type: DateTime, desc: '开始日期', documentation: { param_type: 'body', example: '2010-02-22' }
+        optional :end_date, type: DateTime, desc: '结束日期', documentation: { param_type: 'body', example: '2024-03-22' }
+        optional :page, type: Integer, default: 1, desc: '页码', documentation: { param_type: 'body' }
+        optional :size, type: Integer, default: 20, desc: '每页条数', documentation: { param_type: 'body' }
+      end
+
+      params :search_grimoire do
+        requires :label, type: String, desc: '仓库或社区地址', documentation: { param_type: 'body', example: 'https://github.com/oss-compass/compass-web-service' }
+        optional :level, type: String, desc: '层级 repo或community', default: 'repo', documentation: { param_type: 'body', example: 'repo' }
+
+        optional :sort, type: String, desc: '排序项', documentation: { param_type: 'body', example: 'grimoire_creation_date' }
+        optional :direction, type: String, desc: '排序方向', documentation: { param_type: 'body', example: 'desc' }
+        optional :begin_date, type: DateTime, desc: '开始日期', documentation: { param_type: 'body', example: '2010-02-22' }
+        optional :end_date, type: DateTime, desc: '结束日期', documentation: { param_type: 'body', example: '2024-03-22' }
+        optional :page, type: Integer, default: 1, desc: '页码', documentation: { param_type: 'body' }
+        optional :size, type: Integer, default: 20, desc: '每页条数', documentation: { param_type: 'body' }
       end
 
       def extract_search_params!(params)
         label = params[:label]
         level = params[:level] || 'repo'
         filter_opts = params[:filter_opts]&.map { |opt| OpenStruct.new(opt) } || []
-        sort_opts = params[:sort_opts]&.map { |opt| OpenStruct.new(opt) } || []
+        if params[:sort] && params[:direction]
+          sort_opts = [OpenStruct.new(type: params[:sort], direction: params[:direction])]
+        else
+          sort_opts = []
+        end
+
         begin_date = params[:begin_date]
         end_date = params[:end_date]
         page = params[:page]
