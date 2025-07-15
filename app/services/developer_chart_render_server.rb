da8e26006c84902868e1e481ade17b03eaf2680e
lisongnan <lcamn7700@outlook.com>
2025-07-15 20:06:22 +0800

    add developer svg
    Signed-off-by: lisongnan <lcamn7700@outlook.com>

---
+++ app/services/developer_chart_render_server.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+class DeveloperChartRenderServer
+  include Common
+  include CompassUtils
+
+  def initialize(params = {})
+    @begin_date = params[:begin_date]
+    @end_date = params[:end_date]
+    @option = params[:option]
+    @width = params[:width] || 800
+    @height = params[:height] || 500
+
+  end
+
+  def render!
+    render_developer_chart!
+  end
+
+  def render_developer_chart!
+    option = @option
+
+    payload = {
+      option: option,
+      width: @width,
+      height: @height,
+      type: "svg"
+    }
+
+    request_svg(
+      payload
+    )
+  end
+
+  def generate_title(metrics, field)
+    if metrics == 'overview'
+      I18n.t("analyze.overview")
+    elsif field == metrics.main_score
+      I18n.t("metrics_models.#{metrics.text_ident}.title")
+    else
+      I18n.t("metrics_models.#{metrics.text_ident}.metrics.#{field}")
+    end
+  end
+
+  def generate_subtext(metrics, field)
+    if metrics == 'overview'
+      ''
+    elsif field == metrics.main_score
+      ''
+    else
+      I18n.t("metrics_models.#{metrics.text_ident}.metrics.#{field}_desc")
+    end
+  end
+
+  private
+
+  def request_svg(payload)
+    Faraday.post(
+      "#{ECHARTS_SERVER}/developer_overview",
+      payload.to_json,
+      { 'Content-Type' => 'application/json' }
+    ).body
+  end
+
+end
