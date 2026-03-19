b6dcc0f7fe22271ec9aadbcf24c44871c83482bf
lisongnan <lcamn7700@outlook.com>
2026-03-19 10:54:11 +0800

    Add supply_chain_security
    Signed-off-by: lisongnan <lcamn7700@outlook.com>

---
+++ app/api/openapi/v3/supply_chain_security/source_management/security_management.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Openapi
+  module V3
+    module SupplyChainSecurity
+      module SourceManagement
+        class SecurityManagement < Grape::API
+          version 'v3', using: :path
+          prefix :api
+          format :json
+
+          helpers Openapi::SharedParams::CustomMetricSearch
+          helpers Openapi::SharedParams::AuthHelpers
+          helpers Openapi::SharedParams::ErrorHelpers
+          helpers Openapi::SharedParams::RestapiHelpers
+
+          rescue_from :all do |e|
+            case e
+            when Grape::Exceptions::ValidationErrors
+              handle_validation_error(e)
+            when SearchFlip::ResponseError
+              handle_open_search_error(e)
+            else
+              handle_generic_error(e)
+            end
+          end
+
+          before { require_token! }
+          before do
+            token = params[:access_token]
+            Openapi::SharedParams::RateLimiter.check_token!(token)
+          end
+          before { save_tracking_api! }
+
+        end
+      end
+    end
+  end
+end
